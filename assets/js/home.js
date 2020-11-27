3aad85a7320d3fc9d59e36820628a6b633ed580b
= <=>
2020-11-27 17:30:13 +0700

    animation header-fixed, drop-menu, banner img

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,28 +7,37 @@ $(document).ready(function(){
             $('.header').removeClass('header-activity');
         }
         if($(window).scrollTop() > 100){
-            $('.btn-scroll').css('display', 'flex');
+            $('.btn-scroll').css({'display':'flex', 'animation':'drop-display 1s forwards ease'});
         }
         else {
-            $('.btn-scroll').css('display', 'none');
+            $('.btn-scroll').css({'animation':'drop-hide 1s forwards ease'})
+        }
+    })
+    $('.btn-scroll').click(()=> {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+    })
+    
+    var flag = 1;
+    $('.mobile-position').click(()=> {
+        //if 0 => mặt trăng hiện là sáng, 1 => mặt trời hiện
+        if(flag == 0) {
+            $('.icon-moon').show();
+            $('.icon-sun').hide();
+            flag = 1;
+        }
+        else {
+            $('.icon-moon').hide();
+            $('.icon-sun').show();
+            flag = 0;
         }
     })
 });
-$('.btn-scroll').click(()=> {
-    $('html, body').animate({ scrollTop: 0 }, 1000);
+var listNavLink = document.querySelectorAll('.nav-link');
+listNavLink.forEach((link)=> {
+    link.addEventListener('click', ()=> {
+        var dropMenu = link.nextElementSibling;
+        dropMenu.classList.add('close');
+    })
 })
-var flag = 1;
-$('.mobile-position').click(()=> {
-    //if 0 => mặt trăng hiện là sáng, 1 => mặt trời hiện
-    if(flag == 0) {
-        $('.icon-moon').show();
-        $('.icon-sun').hide();
-        flag = 1;
-        console.log(flag);
-    }
-    else {
-        $('.icon-moon').hide();
-        $('.icon-sun').show();
-        flag = 0;
-    }
-})
+
+   
