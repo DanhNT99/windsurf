84dc931869167e2cc2894aa919430aeaae942763
= <=>
2021-01-03 22:43:51 +0700

    edit code class page home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,38 +1,105 @@
 $(document).ready(function(){
+
+  //window sroll change color menu and show btnTop
     $(window).scroll(()=> {
+
         if($(window).scrollTop() > 0) {
-            $('.header').addClass('header-activity');
+            $('.header').addClass('headerActivity');
         }
         else {
-            $('.header').removeClass('header-activity');
+            $('.header').removeClass('headerActivity');
         }
+
         if($(window).scrollTop() > 100){
-            $('.btn-scroll').css({'display':'flex', 'animation':'animateShow 0.5s forwards ease'});
+            $('.btnScrollTop').css({'display':'flex', 'animation':'animateShow 0.5s forwards ease'});
         }
         else {
-            $('.btn-scroll').css({'display': 'none'})
+            $('.btnScrollTop').css({'display': 'none'})
         }
+
     })
-    $('.btn-scroll').click(()=> {
+    //end
+
+    //click btn để màn hình scroll lên top 0
+    $('.btnScrollTop').click(()=> {
+
         $('html, body').animate({ scrollTop: 0 }, 1000);
+
     })
-    
-    var flag = 1;
-    $('.mobile-position').click(()=> {
-        //if 0 => mặt trăng hiện là sáng, 1 => mặt trời hiện
-        if(flag == 0) {
-            $('.icon-moon').show();
-            $('.icon-sun').hide();
-            flag = 1;
-        }
-        else {
-            $('.icon-moon').hide();
-            $('.icon-sun').show();
-            flag = 0;
-        }
+    //end
+
+    //click show menu mobile
+    $('.header__bottomBtnBar').click(()=> {
+
+      $('.header__bottomBtnBar .show').toggle();
+
+      $('.header__bottomBtnBar .close').toggle();
+
+      $('.header__bottomMenu').toggleClass('headerMenuToRight');
+
+      $('body').toggleClass('closeScrollBody');
+
+    })
+    //end
+
+    //sliderToggle of menu dropdown in mobile
+    if($(window).width() < 991) {
+
+      $('.header__bottomMenuListItem').removeClass('showAllMenuDrop');
+
+      $('.header__bottomMenuListItemIcon').click(function(){
+
+        $(this).next('.header__bottomMenuDropdown').slideToggle();
+
+      })
+
+    }
+    // end
+
+    //click show input in menu
+    $('.header__bottomMenuBtnSearch').click(()=> {
+      $('.header__bottomMenu').addClass('showInput');
+      $('.header__bottomMenuInputSearch').focus();
     })
 
-    $('.showSildeMain').slick({
+    $(window).on('click',function(event){
+      var clickNameClass =  '.' + event.target.className;
+      var flag = $(clickNameClass).hasClass('header__bottomMenuBtnSearch');
+      if(!flag) {
+        $('.header__bottomMenu').removeClass('showInput');
+      }
+    })
+
+    //ẩn hiện icon sun và moon
+    //end
+    $('.header__bottom-icon').click(()=> {
+      $('[bg ="changeColor"]').css({'background': '#111'});
+    })
+
+    //click btn mouse on bannber to scroll down
+    $('.banner__bottom').click(()=> {
+      let bannerHieght = $(".banner").outerHeight();
+      let headerHeight = $('.header').outerHeight();
+      $('html, body').animate({ scrollTop: bannerHieght - headerHeight }, 1000);
+    })
+    //end
+
+    //show and close video
+    function toggleClassVideo() {
+      $('.video__containsClip').toggleClass('showClip');
+      $('.video__overlayClip').toggleClass('showOverlay');
+      $('body').toggleClass('closeScrollBody');
+    }
+    $('.video__rightBtn').on('click', ()=>{
+        toggleClassVideo();
+    })
+    //close video
+    $('.video__overlayClipBtnClose').on('click',()=> {
+      toggleClassVideo();
+    })
+
+    //slider of member
+    $('.showSildeMember').slick({
         dots: true,
         speed: 500,
         slidesToShow:2, 
@@ -70,30 +137,50 @@ $(document).ready(function(){
               }
             }
         ]
-      });
-    
-    // $('.dropdown').on('show.bs.dropdown', function () {
-    //     //trước khi hiện;
-    //     let dropdownMenu = $('.dropdown > div.dropdown-menu');
-    //         dropdownMenu.removeClass('close');
-    //         dropdownMenu.removeClass('none');
-    //   })
+    });
 
-    // $('.dropdown').on('hide.bs.dropdown', function () {
-    //     //trước khi ẩn
-    //     let dropdownMenu = $('.dropdown > div.dropdown-menu.show')
-    //         dropdownMenu.addClass("close");
-    //     $('div.dropdown-menu').on('transitionend', function () {
-    //             $('div.dropdown-menu').addClass('none');
-    //     })
-    //   })
 
+    // slider of post
+    $('.showSilderPost').slick({
+      dots: true,
+      speed: 500,
+      slidesToShow:1, 
+      slidesToScroll:1,
+      prevArrow : false, 
+      // prevArrow: '<button type="button" class="btnSliderSlickPrev prev"><i class="fas fa-arrow-left"></i></button>',
+      nextArrow: '<button type="button" class="btnSliderSlickNext next"><i class="fas fa-arrow-right"></i></button>',
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+      ]
+  });
 })
-// var listNavLink = document.querySelectorAll('.nav-link');
-// listNavLink.forEach((link)=> {
-//     link.addEventListener('click', ()=> {
-//         var dropMenu = link.nextElementSibling;
-//         dropMenu.classList.add('close');
-//     })
-// })
 
